de60f03491637d9f366a3ea88cd2abe3859daa4b
MacsDickinson <macs.dickinson@live.co.uk>
2016-10-09 00:33:01 +0100

    specify timezones

--- src/getRemainingTime.js
+++ src/getRemainingTime.js
@@ -3,7 +3,7 @@ require('moment-precise-range-plugin');
 
 const getRemainingTime = (end) => {
   const london = end.tz('Europe/London');
-  const diff = london.diff(moment());
+  const diff = london.diff(moment().tz('Europe/London'));
   return moment.preciseDiff(0, diff);
 };
 
