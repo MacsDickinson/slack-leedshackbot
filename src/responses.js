d196ef6ccb7cbd9e1113ed82fc4c7610b048822a
MacsDickinson <macs.dickinson@live.co.uk>
2016-10-09 01:08:01 +0100

    added some more responses

--- src/responses.js
+++ src/responses.js
@@ -3,9 +3,49 @@
 const moment = require('moment');
 const getRemainingTime = require('./getRemainingTime');
 
+function isInPast(date) {
+  return date.diff(moment().utcOffset(60)) < 0;
+}
+
+function hackTimeRemaining() {
+  const hackEnd = moment('2016-10-09').utcOffset(60).startOf('day').add(13, 'hours');
+
+  if (isInPast(hackEnd)) {
+    return 'The hack has ended :(';
+  }
+
+  const timeRemaining = getRemainingTime(hackEnd);
+  return `there are ${timeRemaining} left in the hackathon`;
+}
+
+function timeUntilBeer() {
+  const baconTime = moment('2016-10-08').utcOffset(60).startOf('day').add(18, 'hours');
+
+  if (isInPast(baconTime)) {
+    return 'Beer is here! Go get some';
+  }
+
+  const timeRemaining = getRemainingTime(baconTime);
+  return `Bacon butties will arrive in ${timeRemaining}`;
+}
+
+function timeUntilBreakfast() {
+  const baconTime = moment('2016-10-09').utcOffset(60).startOf('day').add(8, 'hours');
+
+  if (isInPast(baconTime)) {
+    return 'It should be here already!';
+  }
+
+  const timeRemaining = getRemainingTime(baconTime);
+  return `Bacon butties will arrive in ${timeRemaining}`;
+}
+
 const responses = [
-  { pattern: /hello/g, response: 'hi' },
-  { pattern: /how long/g, response: getRemainingTime(moment('2016-10-09').utcOffset(60).startOf('day').add(13, 'hours')) },
+  { pattern: /(hello|hi|hey|hola)/g, response: 'hi' },
+  { pattern: /how long until (breakfast|food)/g, response: timeUntilBreakfast() },
+  { pattern: /how long until beer/g, response: timeUntilBeer() },
+  { pattern: /how long/g, response: hackTimeRemaining() },
+  { pattern: /should I go to sleep\?/g, response: 'Probably' },
   { pattern: /server time/g, response: moment().format() }
 ];
 
